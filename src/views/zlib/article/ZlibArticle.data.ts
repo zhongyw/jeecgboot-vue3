49d5ea7c228e7dab4d41f468e2fdd45a3885d34d
zhongyw <6210476@qq.com>
2023-02-09 16:38:56 +0800

    zlib article

--- src/views/zlib/article/ZlibArticle.data.ts
+++ src/views/zlib/article/ZlibArticle.data.ts
@@ -1,77 +1,82 @@
-import {BasicColumn} from '/@/components/Table';
-import {FormSchema} from '/@/components/Table';
+import { h } from 'vue';
+import { BasicColumn } from '/@/components/Table';
+import { FormSchema } from '/@/components/Table';
+import { render } from '/@/utils/common/renderUtils';
 //列表数据
 export const columns: BasicColumn[] = [
-   {
+  {
     title: '标题',
-    align:"center",
-    dataIndex: 'titile'
-   },
-   {
+    align: 'center',
+    dataIndex: 'titile',
+  },
+  {
     title: '内容',
-    align:"center",
-    dataIndex: 'content'
-   },
-   {
+    align: 'center',
+    width: 400,
+    dataIndex: 'content',
+    customRender: ({ text }) => {
+      return h('div', { style: 'max-height: 200px; overflow: hidden', innerHTML: text });
+    },
+  },
+  {
     title: '发布人',
-    align:"center",
-    dataIndex: 'sender'
-   },
-   {
-    title: '优先级（L低，M中，H高）',
-    align:"center",
-    dataIndex: 'priority'
-   },
-   {
-    title: '文章对象类型（note:笔记，collect:采集）',
-    align:"center",
-    dataIndex: 'articleType'
-   },
-   {
-    title: '发布状态（0未发布，1已发布，2已撤销）',
-    align:"center",
-    dataIndex: 'pubStatus'
-   },
-   {
+    align: 'center',
+    dataIndex: 'sender',
+  },
+  {
+    title: '优先级',
+    align: 'center',
+    dataIndex: 'priority',
+  },
+  {
+    title: '文章类型',
+    align: 'center',
+    dataIndex: 'articleType',
+  },
+  {
+    title: '发布状态',
+    align: 'center',
+    dataIndex: 'pubStatus',
+  },
+  {
     title: '发布时间',
-    align:"center",
+    align: 'center',
     dataIndex: 'pubTime',
-    customRender:({text}) =>{
-      return !text?"":(text.length>10?text.substr(0,10):text)
+    customRender: ({ text }) => {
+      return !text ? '' : text.length > 10 ? text.substr(0, 10) : text;
     },
-   },
-   {
+  },
+  {
     title: '撤销时间',
-    align:"center",
+    align: 'center',
     dataIndex: 'cancelTime',
-    customRender:({text}) =>{
-      return !text?"":(text.length>10?text.substr(0,10):text)
+    customRender: ({ text }) => {
+      return !text ? '' : text.length > 10 ? text.substr(0, 10) : text;
     },
-   },
-   {
-    title: '删除状态（0，正常，1已删除）',
-    align:"center",
-    dataIndex: 'delFlag'
-   },
-   {
-    title: '打开方式(组件：component 路由：url)',
-    align:"center",
-    dataIndex: 'openType'
-   },
-   {
+  },
+  {
+    title: '删除状态',
+    align: 'center',
+    dataIndex: 'delFlag',
+  },
+  {
+    title: '打开方式',
+    align: 'center',
+    dataIndex: 'openType',
+  },
+  {
     title: '组件/路由 地址',
-    align:"center",
-    dataIndex: 'openPage'
-   },
-   {
+    align: 'center',
+    dataIndex: 'openPage',
+  },
+  {
     title: '摘要',
-    align:"center",
-    dataIndex: 'articleAbstract'
-   },
+    align: 'center',
+    dataIndex: 'articleAbstract',
+  },
 ];
 //查询数据
-export const searchFormSchema: FormSchema[] = [
-];
+export const searchFormSchema: FormSchema[] = [];
 //表单数据
 export const formSchema: FormSchema[] = [
   {
@@ -82,74 +87,42 @@ export const formSchema: FormSchema[] = [
   {
     label: '内容',
     field: 'content',
-    component: 'InputTextArea',
-  },
-  {
-    label: '发布人',
-    field: 'sender',
     component: 'Input',
+    render: render.renderTinymce,
   },
   {
-    label: '优先级（L低，M中，H高）',
-    field: 'priority',
-    component: 'Input',
-  },
-  {
-    label: '文章对象类型（note:笔记，collect:采集）',
+    label: '文章类型',
     field: 'articleType',
-    component: 'Input',
-  },
-  {
-    label: '发布状态（0未发布，1已发布，2已撤销）',
-    field: 'pubStatus',
-    component: 'Input',
-  },
-  {
-    label: '发布时间',
-    field: 'pubTime',
-    component: 'DatePicker',
-  },
-  {
-    label: '撤销时间',
-    field: 'cancelTime',
-    component: 'DatePicker',
-  },
-  {
-    label: '删除状态（0，正常，1已删除）',
-    field: 'delFlag',
-    component: 'Input',
-  },
-  {
-    label: '打开方式(组件：component 路由：url)',
-    field: 'openType',
-    component: 'Input',
-  },
-  {
-    label: '组件/路由 地址',
-    field: 'openPage',
-    component: 'Input',
+    component: 'Select',
+    componentProps: {
+      options: [
+        {
+          label: '笔记',
+          value: 'note',
+          key: '1',
+        },
+      ],
+    },
   },
   {
     label: '摘要',
     field: 'articleAbstract',
     component: 'InputTextArea',
   },
-	// TODO 主键隐藏字段，目前写死为ID
-	{
-	  label: '',
-	  field: 'id',
-	  component: 'Input',
-	  show: false
-	},
+  // TODO 主键隐藏字段，目前写死为ID
+  {
+    label: '',
+    field: 'id',
+    component: 'Input',
+    show: false,
+  },
 ];
 
-
-
 /**
-* 流程表单调用这个方法获取formSchema
-* @param param
-*/
-export function getBpmFormSchema(_formData): FormSchema[]{
+ * 流程表单调用这个方法获取formSchema
+ * @param param
+ */
+export function getBpmFormSchema(_formData): FormSchema[] {
   // 默认和原始表单保持一致 如果流程中配置了权限数据，这里需要单独处理formSchema
   return formSchema;
 }
--- src/views/zlib/article/ZlibArticleList.vue
+++ src/views/zlib/article/ZlibArticleList.vue
@@ -1,173 +1,170 @@
 <template>
-  <div>
+  <div class="zlib-article-list">
     <!--引用表格-->
-   <BasicTable @register="registerTable" :rowSelection="rowSelection">
-     <!--插槽:table标题-->
+    <BasicTable @register="registerTable" :rowSelection="rowSelection">
+      <!--插槽:table标题-->
       <template #tableTitle>
-          <a-button type="primary" @click="handleAdd" preIcon="ant-design:plus-outlined"> 新增</a-button>
-          <a-button  type="primary" preIcon="ant-design:export-outlined" @click="onExportXls"> 导出</a-button>
-          <j-upload-button  type="primary" preIcon="ant-design:import-outlined" @click="onImportXls">导入</j-upload-button>
-          <a-dropdown v-if="selectedRowKeys.length > 0">
-              <template #overlay>
-                <a-menu>
-                  <a-menu-item key="1" @click="batchHandleDelete">
-                    <Icon icon="ant-design:delete-outlined"></Icon>
-                    删除
-                  </a-menu-item>
-                </a-menu>
-              </template>
-              <a-button>批量操作
-                <Icon icon="mdi:chevron-down"></Icon>
-              </a-button>
+        <a-button type="primary" @click="handleAdd" preIcon="ant-design:plus-outlined"> 新增</a-button>
+        <a-button type="primary" preIcon="ant-design:export-outlined" @click="onExportXls"> 导出</a-button>
+        <j-upload-button type="primary" preIcon="ant-design:import-outlined" @click="onImportXls">导入</j-upload-button>
+        <a-dropdown v-if="selectedRowKeys.length > 0">
+          <template #overlay>
+            <a-menu>
+              <a-menu-item key="1" @click="batchHandleDelete">
+                <Icon icon="ant-design:delete-outlined" />
+                删除
+              </a-menu-item>
+            </a-menu>
+          </template>
+          <a-button
+            >批量操作
+            <Icon icon="mdi:chevron-down" />
+          </a-button>
         </a-dropdown>
       </template>
-       <!--操作栏-->
+      <!--操作栏-->
       <template #action="{ record }">
-        <TableAction :actions="getTableAction(record)" :dropDownActions="getDropDownAction(record)"/>
+        <TableAction :actions="getTableAction(record)" :dropDownActions="getDropDownAction(record)" />
       </template>
       <!--字段回显插槽-->
-      <template #htmlSlot="{text}">
-         <div v-html="text"></div>
+      <template #htmlSlot="{ text }">
+        <div v-html="text"></div>
       </template>
       <!--省市区字段回显插槽-->
-      <template #pcaSlot="{text}">
-         {{ getAreaTextByCode(text) }}
+      <template #pcaSlot="{ text }">
+        {{ getAreaTextByCode(text) }}
       </template>
-      <template #fileSlot="{text}">
-         <span v-if="!text" style="font-size: 12px;font-style: italic;">无文件</span>
-         <a-button v-else :ghost="true" type="primary" preIcon="ant-design:download-outlined" size="small" @click="downloadFile(text)">下载</a-button>
+      <template #fileSlot="{ text }">
+        <span v-if="!text" style="font-size: 12px; font-style: italic">无文件</span>
+        <a-button v-else :ghost="true" type="primary" preIcon="ant-design:download-outlined" size="small" @click="downloadFile(text)">下载</a-button>
       </template>
     </BasicTable>
     <!-- 表单区域 -->
-    <ZlibArticleModal @register="registerModal" @success="handleSuccess"></ZlibArticleModal>
+    <ZlibArticleModal @register="registerModal" @success="handleSuccess" />
   </div>
 </template>
 
 <script lang="ts" name="zlib.zlib-zlibArticle" setup>
-  import {ref, computed, unref} from 'vue';
-  import {BasicTable, useTable, TableAction} from '/@/components/Table';
-  import {useModal} from '/@/components/Modal';
-  import { useListPage } from '/@/hooks/system/useListPage'
-  import ZlibArticleModal from './components/ZlibArticleModal.vue'
-  import {columns, searchFormSchema} from './ZlibArticle.data';
-  import {list, deleteOne, batchDelete, getImportUrl,getExportUrl} from './ZlibArticle.api';
+  import { ref, computed, unref } from 'vue';
+  import { BasicTable, useTable, TableAction } from '/@/components/Table';
+  import { useModal } from '/@/components/Modal';
+  import { useListPage } from '/@/hooks/system/useListPage';
+  import ZlibArticleModal from './components/ZlibArticleModal.vue';
+  import { columns, searchFormSchema } from './ZlibArticle.data';
+  import { list, deleteOne, batchDelete, getImportUrl, getExportUrl } from './ZlibArticle.api';
   import { downloadFile } from '/@/utils/common/renderUtils';
   const checkedKeys = ref<Array<string | number>>([]);
   //注册model
-  const [registerModal, {openModal}] = useModal();
+  const [registerModal, { openModal }] = useModal();
+  
   //注册table数据
-  const { prefixCls,tableContext,onExportXls,onImportXls } = useListPage({
-      tableProps:{
-           title: 'zlib_article',
-           api: list,
-           columns,
-           canResize:false,
-           formConfig: {
-              //labelWidth: 120,
-              schemas: searchFormSchema,
-              autoSubmitOnEnter:true,
-              showAdvancedButton:true,
-              fieldMapToNumber: [
-              ],
-              fieldMapToTime: [
-              ],
-            },
-           actionColumn: {
-               width: 120,
-               fixed:'right'
-            },
+  const { prefixCls, tableContext, onExportXls, onImportXls } = useListPage({
+    tableProps: {
+      title: 'zlib_article',
+      api: list,
+      columns,
+      canResize: false,
+      formConfig: {
+        //labelWidth: 120,
+        schemas: searchFormSchema,
+        autoSubmitOnEnter: true,
+        showAdvancedButton: true,
+        fieldMapToNumber: [],
+        fieldMapToTime: [],
       },
-       exportConfig: {
-            name:"zlib_article",
-            url: getExportUrl,
-          },
-          importConfig: {
-            url: getImportUrl,
-            success: handleSuccess
-          },
-  })
+      actionColumn: {
+        width: 120,
+        fixed: 'right',
+      },
+    },
+    exportConfig: {
+      name: 'zlib_article',
+      url: getExportUrl,
+    },
+    importConfig: {
+      url: getImportUrl,
+      success: handleSuccess,
+    },
+  });
 
-  const [registerTable, {reload},{ rowSelection, selectedRowKeys }] = tableContext
+  const [registerTable, { reload }, { rowSelection, selectedRowKeys }] = tableContext;
 
-   /**
-    * 新增事件
-    */
+  /**
+   * 新增事件
+   */
   function handleAdd() {
-     openModal(true, {
-       isUpdate: false,
-       showFooter: true,
-     });
+    openModal(true, {
+      isUpdate: false,
+      showFooter: true,
+    });
   }
-   /**
-    * 编辑事件
-    */
+  /**
+   * 编辑事件
+   */
   function handleEdit(record: Recordable) {
-     openModal(true, {
-       record,
-       isUpdate: true,
-       showFooter: true,
-     });
-   }
-   /**
-    * 详情
+    openModal(true, {
+      record,
+      isUpdate: true,
+      showFooter: true,
+    });
+  }
+  /**
+   * 详情
    */
   function handleDetail(record: Recordable) {
-     openModal(true, {
-       record,
-       isUpdate: true,
-       showFooter: false,
-     });
-   }
-   /**
-    * 删除事件
-    */
+    openModal(true, {
+      record,
+      isUpdate: true,
+      showFooter: false,
+    });
+  }
+  /**
+   * 删除事件
+   */
   async function handleDelete(record) {
-     await deleteOne({id: record.id}, handleSuccess);
-   }
-   /**
-    * 批量删除事件
-    */
+    await deleteOne({ id: record.id }, handleSuccess);
+  }
+  /**
+   * 批量删除事件
+   */
   async function batchHandleDelete() {
-     await batchDelete({ids: selectedRowKeys.value}, handleSuccess);
-   }
-   /**
-    * 成功回调
-    */
+    await batchDelete({ ids: selectedRowKeys.value }, handleSuccess);
+  }
+  /**
+   * 成功回调
+   */
   function handleSuccess() {
-      (selectedRowKeys.value = []) && reload();
-   }
-   /**
-      * 操作栏
-      */
-  function getTableAction(record){
-       return [
-         {
-           label: '编辑',
-           onClick: handleEdit.bind(null, record),
-         }
-       ]
-   }
-     /**
-        * 下拉操作栏
-        */
-  function getDropDownAction(record){
-       return [
-         {
-           label: '详情',
-           onClick: handleDetail.bind(null, record),
-         }, {
-           label: '删除',
-           popConfirm: {
-             title: '是否确认删除',
-             confirm: handleDelete.bind(null, record),
-           }
-         }
-       ]
-   }
-
-
+    (selectedRowKeys.value = []) && reload();
+  }
+  /**
+   * 操作栏
+   */
+  function getTableAction(record) {
+    return [
+      {
+        label: '编辑',
+        onClick: handleEdit.bind(null, record),
+      },
+    ];
+  }
+  /**
+   * 下拉操作栏
+   */
+  function getDropDownAction(record) {
+    return [
+      {
+        label: '详情',
+        onClick: handleDetail.bind(null, record),
+      },
+      {
+        label: '删除',
+        popConfirm: {
+          title: '是否确认删除',
+          confirm: handleDelete.bind(null, record),
+        },
+      },
+    ];
+  }
 </script>
 
-<style scoped>
-
-</style>
+<style lang="less" scoped></style>
